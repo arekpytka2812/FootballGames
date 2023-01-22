24b396465b2faede02fa34448b6d661cadab45d4
szatk <szatkowski.a.ta@gmail.com>
2023-01-22 19:10:53 +0100

    Fixed pages positions for user and admin, added tableviews and some buttons

--- src/main/java/com/fg/footballgames/User/Controllers/UserCoachesCenterController.java
+++ src/main/java/com/fg/footballgames/User/Controllers/UserCoachesCenterController.java
@@ -1,5 +1,14 @@
 package com.fg.footballgames.User.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class UserCoachesCenterController {
     // TODO - make tables with all coaches
+    @FXML
+    TableView coachesViewTableView;
+    @FXML
+    private void initialize(){
+
+    }
 }
--- src/main/java/com/fg/footballgames/User/Controllers/UserGoalDiffCenterController.java
+++ src/main/java/com/fg/footballgames/User/Controllers/UserGoalDiffCenterController.java
@@ -1,5 +1,15 @@
 package com.fg.footballgames.User.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class UserGoalDiffCenterController {
+    @FXML
+    TableView goalDiffTableView;
+
+    @FXML
+    private void initialize(){
+
+    }
     // TODO - make tables with all goal differences
 }
